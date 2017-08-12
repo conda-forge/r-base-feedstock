aeb16501a6b9dfbd2b4264b8080f98f9df0ce5ef
Travis-CI on github.com/conda-forge/staged-recipes <conda-forge@googlegroups.com>
2017-08-12 20:22:51 +0000

    Re-render the feedstock after CI registration.

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -24,16 +24,30 @@ show_channel_urls: true
 CONDARC
 )
 
+# In order for the conda-build process in the container to write to the mounted
+# volumes, we need to run with the same id as the host machine, which is
+# normally the owner of the mounted volumes, or at least has write permission
+HOST_USER_ID=$(id -u)
+# Check if docker-machine is being used (normally on OSX) and get the uid from
+# the VM
+if hash docker-machine 2> /dev/null && docker-machine active > /dev/null; then
+    HOST_USER_ID=$(docker-machine ssh $(docker-machine active) id -u)
+fi
+
 rm -f "$FEEDSTOCK_ROOT/build_artefacts/conda-forge-build-done"
 
 cat << EOF | docker run -i \
                         -v "${RECIPE_ROOT}":/recipe_root \
                         -v "${FEEDSTOCK_ROOT}":/feedstock_root \
+                        -e HOST_USER_ID="${HOST_USER_ID}" \
                         -a stdin -a stdout -a stderr \
                         condaforge/linux-anvil \
                         bash || exit 1
 
+set -e
+set +x
 export BINSTAR_TOKEN=${BINSTAR_TOKEN}
+set -x
 export PYTHONUNBUFFERED=1
 
 echo "$config" > ~/.condarc
@@ -48,12 +62,12 @@ source run_conda_forge_build_setup
 # "recipe/yum_requirements.txt" file. After updating that file,
 # run "conda smithy rerender" and this line be updated
 # automatically.
-yum install -y mesa-libGL-devel mesa-libGLU-devel libX11-devel libXt-devel libXrender-devel libXext-devel libXdmcp-devel
+/usr/bin/sudo -n yum install -y mesa-libGL-devel mesa-libGLU-devel libX11-devel libXt-devel libXrender-devel libXext-devel libXdmcp-devel
 
 
-# Embarking on 1 case(s).
-    conda build /recipe_root --quiet || exit 1
-    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
+conda build /recipe_root --quiet || exit 1
+upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
+
 touch /feedstock_root/build_artefacts/conda-forge-build-done
 EOF
 
