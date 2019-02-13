adade0bd5b0fc3f4e488094ec395deedb5c51b0e
Fabien Pertuy <PertuyF@users.noreply.github.com>
2019-02-13 14:39:35 +0100

    Restore `deactivate-r-base.sh`
    Restores deactivation script which was lost since commit 6b81c0f

--- recipe/build.sh
+++ recipe/build.sh
@@ -484,10 +484,14 @@ if [[ $target_platform == osx-64 ]]; then
   Darwin
   mkdir -p ${PREFIX}/etc/conda/activate.d
   cp "${RECIPE_DIR}"/activate-${PKG_NAME}.sh ${PREFIX}/etc/conda/activate.d/activate-${PKG_NAME}.sh
+  mkdir -p ${PREFIX}/etc/conda/deactivate.d
+  cp "${RECIPE_DIR}"/deactivate-${PKG_NAME}.sh ${PREFIX}/etc/conda/deactivate.d/deactivate-${PKG_NAME}.sh
 elif [[ $target_platform =~ .*linux.* ]]; then
   Linux
   mkdir -p ${PREFIX}/etc/conda/activate.d
   cp "${RECIPE_DIR}"/activate-${PKG_NAME}.sh ${PREFIX}/etc/conda/activate.d/activate-${PKG_NAME}.sh
+  mkdir -p ${PREFIX}/etc/conda/deactivate.d
+  cp "${RECIPE_DIR}"/deactivate-${PKG_NAME}.sh ${PREFIX}/etc/conda/deactivate.d/deactivate-${PKG_NAME}.sh
 elif [[ $(uname) =~ M.* ]]; then
   # Mingw_w64_autotools
   Mingw_w64_makefiles
