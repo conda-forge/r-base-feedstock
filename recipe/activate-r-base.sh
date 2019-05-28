0c6ebf427dedd158e3c96615fe9baa65b615f76e
John Blischak <jdblischak@gmail.com>
2019-05-28 16:37:22 -0400

    Check env var CONDA_KEEP_R_LIBS_USER before removing R_LIBS_USER.
    This allows the user to continue using a custom user library inside of
    the conda environment.

--- recipe/activate-r-base.sh
+++ recipe/activate-r-base.sh
@@ -7,8 +7,8 @@ if [[ ! -z ${RSTUDIO_WHICH_R+x} ]]; then
 fi
 export RSTUDIO_WHICH_R="$CONDA_PREFIX/bin/R"
 
-# store existing R_LIBS_USER
-if [[ ! -z ${R_LIBS_USER+x} ]]; then
+# store existing R_LIBS_USER if the user has not set CONDA_KEEP_R_LIBS_USER
+if [[ ! -z ${R_LIBS_USER+x} && -z ${CONDA_KEEP_R_LIBS_USER+x} ]]; then
   export R_LIBS_USER_PREV="$R_LIBS_USER"
 fi
 unset R_LIBS_USER
