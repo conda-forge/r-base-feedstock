18581d3789903fcde57be4ad0628d997e992ef05
Marcel Bargull <marcel.bargull@udo.edu>
2019-06-18 00:01:18 +0200

    Windows/MiKTeX: maybe --no-registry? IDK what stalls...

--- recipe/build.sh
+++ recipe/build.sh
@@ -331,7 +331,7 @@ Mingw_w64_makefiles() {
       # http://ctan.mines-albi.fr/systems/win32/miktex/tm/packages/inconsolata.tar.lzma
         curl --insecure -C - -o ${DLCACHE}/basic-miktex-${MIKTEX_VER}.exe -SL https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x86/basic-miktex-${MIKTEX_VER}.exe || true
         echo "Extracting basic-miktex-${MIKTEX_VER}.exe, this will take some time ..."
-        ${DLCACHE}/basic-miktex-${MIKTEX_VER}.exe --portable=${PWD} --unattended &
+        ${DLCACHE}/basic-miktex-${MIKTEX_VER}.exe --portable=${PWD} --unattended --no-registry &
         local miktex_setup_pid=$!
         while kill -0 "${miktex_setup_pid}" >/dev/null 2>&1 ; do
           du -sc ./*
