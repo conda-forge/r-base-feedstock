c4b8b9074550f8e70f3bb08d5c80f8d5f74a92e6
Matthias Bernt <m.bernt@ufz.de>
2020-06-03 13:49:34 +0200

    add test for png generation

--- recipe/test-svg.R
+++ recipe/test-svg.R
@@ -4,3 +4,10 @@ dev.off()
 if (!file.exists("test.svg")) {
     stop("SVG not created!")
 }
+png(filename="test.png")
+pie( 1:10, labels=paste("label number",1:10))
+dev.off()
+if (!file.exists("test.png")) {
+    stop("PNG not created!")
+}
+
