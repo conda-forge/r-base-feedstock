8ad2c0f96ff56b1908dff360b8ba46605d017784
Isuru Fernando <isuruf@gmail.com>
2020-12-03 10:31:22 -0800

    Fix testing cross-r-base

--- recipe/activate-cross-r-base.sh
+++ recipe/activate-cross-r-base.sh
@@ -1,4 +1,6 @@
-export R=$BUILD_PREFIX/bin/R
-export R_ARGS="--library=$PREFIX/lib/R/library --no-test-load"
-echo "R_HOME=$PREFIX/lib/R"     > $PREFIX/lib/R/etc/Makeconf
-cat $PREFIX/lib/R/etc/Makeconf >> $PREFIX/lib/R/etc/Makeconf
+if [[ "$CONDA_BUILD_STATE" != "test" && "$build_platform" != "$target_platform" ]]; then
+  export R=$BUILD_PREFIX/bin/R
+  export R_ARGS="--library=$PREFIX/lib/R/library --no-test-load"
+  echo "R_HOME=$PREFIX/lib/R"     > $PREFIX/lib/R/etc/Makeconf
+  cat $PREFIX/lib/R/etc/Makeconf >> $PREFIX/lib/R/etc/Makeconf
+fi
