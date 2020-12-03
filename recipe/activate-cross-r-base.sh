8afd2c5d498638faec9ce3adec72e41356df9e30
Isuru Fernando <isuruf@gmail.com>
2020-12-03 14:04:58 -0600

    Fix prefix

--- recipe/activate-cross-r-base.sh
+++ recipe/activate-cross-r-base.sh
@@ -1,8 +1,8 @@
 if [[ "$CONDA_BUILD_STATE" != "test" && "$build_platform" != "$target_platform" ]]; then
   export R=$BUILD_PREFIX/bin/R
   export R_ARGS="--library=$PREFIX/lib/R/library --no-test-load"
-  echo "R_HOME=$PREFIX/lib/R"     > $PREFIX/lib/R/etc/Makeconf
-  cat $PREFIX/lib/R/etc/Makeconf >> $PREFIX/lib/R/etc/Makeconf
+  echo "R_HOME=$PREFIX/lib/R"     > $BUILD_PREFIX/lib/R/etc/Makeconf
+  cat $PREFIX/lib/R/etc/Makeconf >> $BUILD_PREFIX/lib/R/etc/Makeconf
 fi
 
 if [[ "$CONDA_BUILD" == "" ]]; then
