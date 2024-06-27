8fbc98ab7d1f08d6a7ba02dea464525a7deb64a9
Isuru Fernando <isuruf@gmail.com>
2024-06-27 14:32:30 -0500

    Merge pull request #313 from mbargull-feedstocks/posix-activation-scripts
    Use POSIX scripts for de-/activation scripts

--- recipe/deactivate-r-base.sh
+++ recipe/deactivate-r-base.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env sh
+# shellcheck shell=sh
 
 # restore pre-existing RSTUDIO_WHICH_R
-if [[ ! -z ${RSTUDIO_WHICH_R_PREV+x} ]]; then
-  export RSTUDIO_WHICH_R="$RSTUDIO_WHICH_R_PREV"
+if [ -n "${RSTUDIO_WHICH_R_PREV+x}" ]; then
+  export RSTUDIO_WHICH_R="${RSTUDIO_WHICH_R_PREV}"
   unset RSTUDIO_WHICH_R_PREV
 else
   unset RSTUDIO_WHICH_R
