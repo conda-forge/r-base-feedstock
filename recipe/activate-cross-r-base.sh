34124f312500889030ce0d94f54926692bed9493
Isuru Fernando <isuruf@gmail.com>
2024-07-27 21:45:53 -0500

    Keep old Makeconf around in cross-r-base

--- recipe/activate-cross-r-base.sh
+++ recipe/activate-cross-r-base.sh
@@ -9,8 +9,11 @@ fi
 if [ "${CONDA_BUILD_STATE-}" != "TEST" ] && [ "${build_platform}" != "${target_platform}" ]; then
   export R="${BUILD_PREFIX}/bin/R"
   export R_ARGS="--library=${PREFIX}/lib/R/library --no-test-load"
+  # Keep these around if we want to build a native version
+  cp "${BUILD_PREFIX}/lib/R/etc/Makeconf" "${BUILD_PREFIX}/lib/R/etc/Makeconf.${build_platform}"
   echo "R_HOME=${PREFIX}/lib/R"       > "${BUILD_PREFIX}/lib/R/etc/Makeconf"
   cat "${PREFIX}/lib/R/etc/Makeconf" >> "${BUILD_PREFIX}/lib/R/etc/Makeconf"
+  cp "${BUILD_PREFIX}/lib/R/etc/Makeconf" "${BUILD_PREFIX}/lib/R/etc/Makeconf.${target_platform}"
   if [ -d "${BUILD_PREFIX}/lib/R/library" ]; then
     rsync -a -I "${BUILD_PREFIX}/lib/R/library/" "${PREFIX}/lib/R/library/"
   fi
