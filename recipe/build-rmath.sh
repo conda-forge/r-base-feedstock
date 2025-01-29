9782b7cb8e3817046f763dc82ca3eb7360c84ed4
Michael Milton <michael.r.milton@gmail.com>
2025-01-29 15:45:09 +1100

    Remove everything from host dependencies, export PKP_CONFIG_PATH

--- recipe/build-rmath.sh
+++ recipe/build-rmath.sh
@@ -1,3 +1,4 @@
+export PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig
 ./configure --prefix=${PREFIX} --without-readline --without-tcltk --without-cairo --without-libpng --without-jpeglib --without-libtiff --without-x --without-libdeflate-compression --disable-R-profiling --disable-memory-profiling --disable-nls --without-ICU
 cd src/nmath
 make
