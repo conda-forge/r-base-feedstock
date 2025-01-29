454fb8a66c4372aff94b76ce1ac06bf3c03a0a03
Michael Milton <michael.r.milton@gmail.com>
2025-01-29 13:32:49 +1100

    Only build for linux, disable as many capabilities as possible in configure

--- recipe/build-rmath.sh
+++ recipe/build-rmath.sh
@@ -1,4 +1,4 @@
-./configure --prefix=${PREFIX}
+./configure --prefix=${PREFIX} --without-readline --without-tcltk --without-cairo --without-libpng --without-jpeglib --without-libtiff --without-x --without-libdeflate-compression --disable-R-profiling --disable-memory-profiling --disable-nls --without-ICU
 cd src/nmath
 make
 cp *.h ${PREFIX}/include
